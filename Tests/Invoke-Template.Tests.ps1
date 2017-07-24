586ae65c686f9b5b13dbc1e87eaef03b4ae0f467
Toffe Bergström <toffe@kabtoffe.net>
2017-07-24 09:52:56 +0300

    Add support for model object

--- Tests/Invoke-Template.Tests.ps1
+++ Tests/Invoke-Template.Tests.ps1
@@ -1,4 +1,4 @@
-﻿Import-Module ..\XrmFunctionGenerator\XrmFunctionGenerator.psd1 -Force
+﻿. ..\XrmFunctionGenerator\Invoke-Template.ps1
 
 Describe "Invoke-Template" {
     It "Returns template" {
@@ -9,5 +9,11 @@ Describe "Invoke-Template" {
     It "Returns template using data" {
         $result = Invoke-Template -Template "`$TestTemplate" -TemplateData @{ "TestTemplate" = "TestData" }
         $result | Should Be "TestData"
+    }
+
+    It "Returns template using data from object" {
+        
+        $result = Invoke-Template -Template "`$TestTemplateFoo;`$Test2" -TemplateModel (New-Object PsObject -Property @{ "TestTemplateFoo" = "TestData2"; "Test2" = "Foo3" })
+        $result | Should Be "TestData2;Foo3"
     }
 }
--- XrmFunctionGenerator/Invoke-Template.ps1
+++ XrmFunctionGenerator/Invoke-Template.ps1
@@ -5,12 +5,21 @@
 
         [string]$Template,
 
-        [hashtable]$TemplateData
+        [hashtable]$TemplateData,
+
+        [pscustomobject]$TemplateModel
     )
 
     #Add template data as variables
     foreach ($key in $TemplateData.Keys){
         Invoke-Expression "`$$key = `$TemplateData[`$key]"
+    }
+
+    if ($TemplateModel -ne $null){
+        #Add template dataobject properties as variables
+        foreach ($key in $TemplateModel | Get-Member -MemberType NoteProperty){
+            Invoke-Expression "`$$($key.Name) = `$TemplateModel.$($key.Name)"
+        }
     }
 
     $TemplateExpression = "@`"`n" + $Template + "`n`"@"
