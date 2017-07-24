33c306227b46b48019a8bcdcfcd0f3f982785a9a
Toffe Bergström <toffe@kabtoffe.net>
2017-07-24 17:54:58 +0300

    Add more integration tests

--- Tests/Integration.Tests.ps1
+++ Tests/Integration.Tests.ps1
@@ -23,7 +23,24 @@ Describe "Test function genereration and use against CRM instance" {
             1 = "Competitor"
             3 = "Customer"
         }
+    },
+    [PSCustomObject]@{
+        "SchemaName" = "parentaccountid"
+        "DisplayName" = "ParentAccount"
+        "AttributeType" = "Lookup"
+        "TargetEntityLogicalName" = "account"
+    },
+    [PSCustomObject]@{
+        "SchemaName" = "createdon"
+        "DisplayName" = "Created"
+        "AttributeType" = "DateTime"
+    },
+    [PSCustomObject]@{
+        "SchemaName" = "address1_latitude"
+        "DisplayName" = "Latitude"
+        "AttributeType" = "Double"
     }
+
     
     "Get","Set","New","Remove" | ForEach-Object { Invoke-Expression (Get-GeneratedXrmFunction -EntityDisplayName Account -EntityLogicalName account -Attributes $Attributes -Template $_) }
     
@@ -43,8 +60,9 @@ Describe "Test function genereration and use against CRM instance" {
         Get-Command Remove-XrmAccount | Should Not Be $null
     }
 
-    It "Account creation works" {
+    It "Can create record" {
         $global:AccountId = New-XrmAccount -Name "Testaccount"
+        $global:ParentAccountId = New-XrmAccount -Name "Testaccount2"
     }
 
     It "Created account exists" {
@@ -52,26 +70,59 @@ Describe "Test function genereration and use against CRM instance" {
         $global:AccountRecord.name | Should Be "Testaccount"
     }
 
-    It "Can update phonenumber" {
+    It "Can update string-value" {
         $global:AccountId | Set-XrmAccount -Phone "911"
         $global:AccountRecord = Get-XrmAccount -AccountId $global:AccountId
         $global:AccountRecord.telephone1 | Should Be "911"
     }
 
-    It "Can update customertypecode" {
+    It "Can update picklist-value" {
         $global:AccountId | Set-XrmAccount -CustomerType Customer
         $global:AccountRecord = Get-XrmAccount -AccountId $global:AccountId
         $global:AccountRecord.customertypecode_Property.Value.Value | Should Be 3
     }
 
-    It "Can query using customertypecode" {
+    It "Can update lookup-value" {
+        $global:AccountId | Set-XrmAccount -ParentAccountId $global:ParentAccountId
+        $global:AccountRecord = Get-XrmAccount -AccountId $global:AccountId -Fields "parentaccountid"
+        $global:AccountRecord.parentaccountid_Property.Value.Id | Should Be $global:ParentAccountId
+    }
+
+    It "Can update Double-value" {
+        $global:AccountId | Set-XrmAccount -Latitude 2.0
+        $global:AccountRecord = Get-XrmAccount -AccountId $global:AccountId
+        $global:AccountRecord.address1_latitude_Property.Value | Should Be 2.00
+    }
+
+    It "Can update DateTime-value" {
+
+    }
+
+    It "Can query picklist-value" {
         $Accounts = Get-XrmAccount -CustomerType Customer -Fields "accountid"
         $Accounts.accountid -contains $global:AccountId | Should Be $true
     }
 
-    It "Removing account works" {
+    It "Can query lookup-value" {
+        $Accounts = Get-XrmAccount -ParentAccountId $global:ParentAccountId
+        $Accounts.accountid -contains $global:AccountId | Should Be $true
+    }
+
+    It "Can query Double-value" {
+        $Accounts = Get-XrmAccount -Latitude 2.0
+        $Accounts.accountid -contains $global:AccountId | Should Be $true
+    }
+
+    It "Can query DateTime-value" {
+        $Accounts = Get-XrmAccount -Created ([DateTime]::Today)
+        $Accounts.accountid -contains $global:AccountId | Should Be $true
+    }
+
+    It "Can remove record" {
         $global:AccountId | Remove-XrmAccount
+        $global:ParentAccountId | Remove-XrmAccount
         { Get-XrmAccount -AccountId $global:AccountId } | Should Throw "account With Id = $($global:AccountId.ToString()) Does Not Exist"
+        { Get-XrmAccount -AccountId $global:ParentAccountId } | Should Throw "account With Id = $($global:ParentAccountId.ToString()) Does Not Exist"
     }
 
 }
